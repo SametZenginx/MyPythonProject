cf6f7777d88d547e9f08e71fc531e23fa9fa4f00
Mustafa Samet <Sametzengin_43@outlook.com>
2022-02-16 17:25:19 +0300

    Create Coding practice 1.py

---
+++ 1. Basic data structures and objects/Part 1 Homeworks/Coding practice 1.py
@@ -0,0 +1,10 @@
+print("Enter the three number: ")
+
+a = int(input("1. :"))
+b = int(input("2. :"))
+c = int(input("3. :"))
+
+Multiplication = a*b*c
+
+print("{}  x {} x {} = {}".format(a,b,c,Multiplication))
+
