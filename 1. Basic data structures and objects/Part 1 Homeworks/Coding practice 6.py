5c9a3f649320bffff0399a336419fd921277589e
Mustafa Samet <Sametzengin_43@outlook.com>
2022-02-16 17:25:33 +0300

    Create Coding practice 6.py

---
+++ 1. Basic data structures and objects/Part 1 Homeworks/Coding practice 6.py
@@ -0,0 +1,9 @@
+print("Enter the two perpendicular sides(a and b) of the triangle and find its hypotenuse.")
+
+a = float(input("a = "))
+b = float(input("b = "))
+
+Hypotenuse = ((a**2) + (b**2)) ** 0.5
+
+print("First edge = {}\nSecond edge = {}\nHypotenuse edge is {}".format(a,b,Hypotenuse))
+
