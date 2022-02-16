fe545ca14795ff1aced430a088887cfc8bf28f3b
Mustafa Samet <Sametzengin_43@outlook.com>
2022-02-16 17:27:25 +0300

    Create SametsModul.py

---
+++ 5. Modules/Part 5 Homeworks/SametsModul.py
@@ -0,0 +1,29 @@
+# Addition, Subtraction, Multiplication, Division, Exponential.
+def Addition(a,b):
+    """
+    This function adds Addition two numbers.
+    """
+    return a+b
+def Subtraction(a,b):
+    """
+    This function subtracts two numbers from each other.
+    """
+    if a > b:
+        return a-b
+    else:
+        return b-a
+def Multiplication(a,b):
+    """
+    This function multiples two numbers.
+    """
+    return a*b
+def Division(a,b):
+    """
+    This function divides two numbers. 
+    """
+    return a/b
+def Exponential(a,b):
+    """
+    This function 1.Number * 1.Number * ...("2.Number") does the operation.
+    """
+    return a**b
